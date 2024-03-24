43b52b8e525eaebaf1dcf3a7975f628ccc54978f
J_powell <157298910+Kittisak008B@users.noreply.github.com>
2024-03-25 00:04:34 +0700

    Create Department Highest Salary.sql

---
+++ LeetCode-Medium/Department Highest Salary.sql
@@ -0,0 +1,65 @@
+-- Table: Employee
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | id           | int     |
+-- | name         | varchar |
+-- | salary       | int     |
+-- | departmentId | int     |
+-- +--------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- departmentId is a foreign key (reference columns) of the ID from the Department table.
+-- Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+-- Table: Department
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table. It is guaranteed that department name is not NULL.
+-- Each row of this table indicates the ID of a department and its name.
+ 
+-- Write a solution to find employees who have the highest salary in each of the departments.
+-- Return the result table in any order.
+
+-- Example 1:
+-- Input: 
+-- Employee table:
+-- +----+-------+--------+--------------+
+-- | id | name  | salary | departmentId |
+-- +----+-------+--------+--------------+
+-- | 1  | Joe   | 70000  | 1            |
+-- | 2  | Jim   | 90000  | 1            |
+-- | 3  | Henry | 80000  | 2            |
+-- | 4  | Sam   | 60000  | 2            |
+-- | 5  | Max   | 90000  | 1            |
+-- +----+-------+--------+--------------+
+-- Department table:
+-- +----+-------+
+-- | id | name  |
+-- +----+-------+
+-- | 1  | IT    |
+-- | 2  | Sales |
+-- +----+-------+
+-- Output: 
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Jim      | 90000  |
+-- | Sales      | Henry    | 80000  |
+-- | IT         | Max      | 90000  |
+-- +------------+----------+--------+
+-- Explanation: Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
+-- Write your PostgreSQL query statement below
+WITH t1 AS (SELECT departmentid , name ,salary 
+            FROM Employee
+            WHERE (departmentId,salary) IN (SELECT departmentid ,MAX(salary) 
+                                            FROM Employee GROUP BY departmentid)
+           )
+SELECT Department.name AS Department, t1.name AS Employee, t1.salary AS Salary 
+FROM t1
+JOIN Department 
+ON Department.id = t1.departmentId
