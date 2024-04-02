6852463ff953f591413b907eb0902e48cf16dc6e
J_powell <157298910+Kittisak008B@users.noreply.github.com>
2024-04-03 01:34:43 +0700

    Create Product Sales Analysis III.sql

---
+++ LeetCode-Medium/Product Sales Analysis III.sql
@@ -0,0 +1,33 @@
+-- Write a solution to select the product id, year, quantity, and price for the first year of every product sold.
+-- Return the resulting table in any order.
+
+-- Example 1:
+-- Input: 
+-- Sales table:
+-- +---------+------------+------+----------+-------+
+-- | sale_id | product_id | year | quantity | price |
+-- +---------+------------+------+----------+-------+ 
+-- | 1       | 100        | 2008 | 10       | 5000  |
+-- | 2       | 100        | 2009 | 12       | 5000  |
+-- | 7       | 200        | 2011 | 15       | 9000  |
+-- +---------+------------+------+----------+-------+
+-- Product table:
+-- +------------+--------------+
+-- | product_id | product_name |
+-- +------------+--------------+
+-- | 100        | Nokia        |
+-- | 200        | Apple        |
+-- | 300        | Samsung      |
+-- +------------+--------------+
+-- Output: 
+-- +------------+------------+----------+-------+
+-- | product_id | first_year | quantity | price |
+-- +------------+------------+----------+-------+ 
+-- | 100        | 2008       | 10       | 5000  |
+-- | 200        | 2011       | 15       | 9000  |
+-- +------------+------------+----------+-------+
+
+-- Write your PostgreSQL query statement below
+SELECT product_id , year first_year , quantity , price  
+FROM Sales 
+WHERE (product_id , year) IN (SELECT product_id ,MIN(year) FROM Sales GROUP BY product_id)
